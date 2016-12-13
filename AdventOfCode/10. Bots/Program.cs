433f5559d2d2073b88b8c8df1068398d611f98f0
Wote <Wote>
2016-12-13 07:20:41 +0200

    Started to rewrote solution

--- AdventOfCode/10. Bots/Program.cs
+++ AdventOfCode/10. Bots/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -8,12 +9,11 @@ namespace _10.Bots
     abstract class Interface
     {
         public Int16 ID;
+        public List<int> values;
     }
 
     class Bot : Interface
     {
-        new public Int16 ID;
-        public Int16[] chips;
         public Interface lower;     
         public Interface higher;
 
@@ -22,15 +22,11 @@ namespace _10.Bots
             ID = id;
             lower = null;
             higher = null;
-            chips = new Int16[2] {Int16.MaxValue, Int16.MaxValue};
         }
     }
 
     class Output : Interface
-    {
-        new public Int16 ID;
-        public List<int> values;
-
+    {        
         public Output(Int16 id)
         {
             ID = id;
@@ -41,117 +37,60 @@ namespace _10.Bots
     class Program
     {
         private static Bot[] bots;
-        private static List<int>[] output;
 
         static void Main(string[] args)
         {
+            var regex = new Regex(@"value (?<value>\d+) goes to bot (?<bot>\d+)|bot (?<from>\d+) gives low to (?<lower>(bot|output)) (?<lowvalue>\d+) and high to (?<higher>(bot|output)) (?<highvalue>\d+)");            
             bots = new Bot[210];
             for (Int16 i = 0; i < bots.Length; i++)
             {
                 bots[i] = new Bot(i);
             }
-            Regex rgx = new Regex(@"(bot|value|output) [0-9]+");
-            Regex rgx2 = new Regex(@"[0-9]+");
+
+            // Initialize situation
             using (StreamReader sr = new StreamReader("input.txt"))
             {
                 while (!sr.EndOfStream)
                 {
-                    string line = sr.ReadLine();
-                    var matches = rgx.Matches(line);
-                    
-                    if (line.StartsWith("bot"))
+                    try
                     {
-                        // Bot value
-                        string botMatch = rgx2.Match(matches[0].Value).Value;
-                        Int16 bot = Int16.Parse(botMatch);
-
-                        // Low value
-                        if (matches[1].Value.Contains("bot"))
+                        string line = sr.ReadLine();
+                        var match = regex.Match(line);
+                        // Add values
+                        if (match.Groups["value"].Success)
                         {
-                            string match = rgx2.Match(matches[1].Value).Value;
-                            bots[bot].lower = bots[Int16.Parse(match)];
+                            bots[short.Parse(match.Groups["from"].Value)].values.Add(short.Parse(match.Groups["value"].Value));
+                        }
+                        else if (match.Groups["from"].Success)
+                        {
+                            if(match.Groups["lower"].Value == "bot")
+                            {
+                                bots[short.Parse(match.Groups["bot"].Value)].lower = bots[short.Parse(match.Groups["lowvalue"].Value)];
+                            }
+                            else
+                            {
+                                bots[short.Parse(match.Groups["bot"].Value)].lower = new Output(short.Parse(match.Groups["lowvalue"].Value));
+                            }
+                            if (match.Groups["higher"].Value == "bot")
+                            {
+                                bots[short.Parse(match.Groups["bot"].Value)].higher = bots[short.Parse(match.Groups["highvalue"].Value)];
+                            }
+                            else
+                            {
+                                bots[short.Parse(match.Groups["bot"].Value)].higher = new Output(short.Parse(match.Groups["highvalue"].Value));
+                            }
                         }
                         else
                         {
-                            string match = rgx2.Match(matches[1].Value).Value;
-                            bots[bot].lower = new Output(Int16.Parse(match));
+                            throw new Exception();
                         }
-
-                        // High value
-                        if (matches[2].Value.Contains("bot"))
-                        {
-                            string match = rgx2.Match(matches[2].Value).Value;
-                            bots[bot].higher = bots[(Int16.Parse(match))];
-                        }
-                        else
-                        {
-                            string match = rgx2.Match(matches[2].Value).Value;
-                            bots[bot].higher = new Output(Int16.Parse(match));
-                        }
-                        bots[bot].CheckStatus(bots);
                     }
-                    else if (line.StartsWith("value"))
+                    catch (Exception ex)
                     {
-                        Int16 value = Int16.Parse(rgx2.Match(matches[0].Value).Value);
-                        Int16 bot = Int16.Parse(rgx2.Match(matches[1].Value).Value);
-                        bots[bot].Add(value, bots);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Weird line...");
+                        Console.WriteLine("Bad input line: " + ex.Message);
                     }
                 }
-            }
-        }
-    }
-
-    static class ExtensionMethods
-    {
-        internal static void Add(this Interface addTo, Int16 value, Bot[] bots)
-        {
-            if (addTo.GetType() == typeof(Bot))
-            {
-                Bot bot = (Bot) addTo;
-                if (bot.chips[0].Equals(Int16.MaxValue))
-                    bot.chips[0] = value;
-                else if (bot.chips[1].Equals(Int16.MaxValue))
-                    bot.chips[1] = value;
-                else
-                    throw new Exception("Full chips..");
-                bot.SortChips();
-                if (bot.chips[0] == 17 && bot.chips[1] == 61)
-                    Console.WriteLine("Bot: " + bot.ID);
-                bot.CheckStatus(bots);
-            }
-            else
-            {
-                Output output = (Output) addTo;
-                output.values.Add(value);
-            }
-        }
-
-        internal static void CheckStatus(this Bot bot, Bot[] bots)
-        {
-            if (bot.higher != null && bot.lower != null && bot.chips[0] != Int16.MaxValue && bot.chips[1] != Int16.MaxValue)
-                bot.Move(bots);
-        }
-
-        internal static void Move(this Bot bot, Bot[] bots)
-        {
-            bot.lower.Add(bot.chips[0], bots);
-            bot.higher.Add(bot.chips[0], bots);
-            bot.chips[0] = Int16.MaxValue;
-            bot.chips[0] = Int16.MaxValue;
-        }
-
-        internal static void SortChips(this Bot bot)
-        {
-            if (bot.chips[0] > bot.chips[1])
-            {
-                bot.chips[0] ^= bot.chips[1];
-                bot.chips[1] ^= bot.chips[0];
-                bot.chips[0] ^= bot.chips[1];
-            }
+            }       
         }
     }
 }
