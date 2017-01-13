d36251fc942fba8fa6db12554bdc4f51fa1824ba
Valtteri Tarvus <valtteri.tarvus@liaison.com>
2017-01-13 23:02:26 +0200

    Solutions 16 and 17

---
+++ AdventOfCode/16. Dragon Curve/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace _16.Dragon_Curve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool test = false;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int phase1Length = 272;
+            int phase2Length = 35651584;
+            string data = args.Length != 0 ? args[0] : "11110010111001001"; // Puzzle input via paramter or hardcoded my default value
+            bool[] array = new bool[data.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = data[i] == '1';
+            }
+
+            while (array.Length <= phase1Length)
+            {
+                array = DragonCurve(array);
+            }
+            CalculateChecksum(array, phase1Length);
+
+            while (array.Length <= phase2Length)
+            {
+                array = DragonCurve(array);
+            }
+            CalculateChecksum(array, phase2Length);
+            
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed.TotalSeconds);
+
+        }
+
+        static bool[] DragonCurve(bool[] array)
+        {
+            bool[] a = array;
+            bool[] b = new bool[a.Length];
+            Array.Copy(a, b, a.Length);
+            Array.Reverse(b);
+            BitArray BitB = new BitArray(b);
+            BitB.Not().CopyTo(b, 0);
+            array = new bool[2 * a.Length + 1];
+            Array.Copy(a, array, a.Length);
+            array[a.Length] = false;
+            Array.Copy(b, 0, array, a.Length + 1, a.Length);
+            return array;
+        }
+
+        static void CalculateChecksum(bool[] array, int length)
+        {
+            char[] checkSumData = new char[length];
+            Array.Copy(Array.ConvertAll(array, x => x ? '1' : '0'), 0, checkSumData, 0, length);
+            char[] checkSum;
+            do
+            {
+                checkSum = new char[checkSumData.Length / 2];
+                for (int i = 0, j = 0; i < checkSumData.Length; i += 2, j++)
+                {
+                    checkSum[j] = checkSumData[i] == checkSumData[i + 1] ? '1' : '0';
+                }
+                checkSumData = new char[checkSum.Length];
+                Array.Copy(checkSum, checkSumData, checkSumData.Length);
+            } while (checkSum.Length % 2 == 0);
+            Console.WriteLine(new string(checkSumData));
+        }
+    }
+}
---
+++ AdventOfCode/17. Route/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _17.Route
+{
+    class Program
+    {
+        static List<KeyValuePair<string, KeyValuePair<int, int>>> possibleRoutes = new List<KeyValuePair<string, KeyValuePair<int, int>>>();
+        static string[] directions = new[] { "U", "D", "L", "R" };
+        
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            string input = args.Length != 0 ? args[0] : "yjjvjgan"; // Puzzle input via paramter or hardcoded my default value
+            string shortestPath = "";
+            int longestPathLength = 0;
+            possibleRoutes.Add(new KeyValuePair<string, KeyValuePair<int,int>>(input, new KeyValuePair<int, int>(0,0)));
+            for(int i = 0; i < possibleRoutes.Count; i++)
+            {
+                bool[] doors = CheckDoors(possibleRoutes[i].Key);
+                for (int j = 0; j < doors.Length; j++)
+                {
+                    if (doors[j] && IsDoor(possibleRoutes[i].Value, j))
+                    {
+                        KeyValuePair<int, int> location = possibleRoutes[i].Value;
+                        if (NewLocation(directions[j], ref location))
+                        {
+                            if(String.IsNullOrEmpty(shortestPath))
+                                shortestPath = (possibleRoutes[i].Key + directions[j]).Substring(input.Length);
+                            longestPathLength = possibleRoutes[i].Key.Length - input.Length + 1;
+                            continue;
+                        }
+                        possibleRoutes.Add(new KeyValuePair<string, KeyValuePair<int, int>>(possibleRoutes[i].Key + directions[j], location));
+                    }
+                }
+            }
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed.TotalSeconds);
+            Console.WriteLine("Shortest path: " + shortestPath);
+            Console.WriteLine("Longest path length: " + longestPathLength);
+        }
+
+        private static bool IsDoor(KeyValuePair<int, int> pair, int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return pair.Key > 0;
+                case 1:
+                    return pair.Key < 3;
+                case 2:
+                    return pair.Value > 0;
+                case 3:
+                    return pair.Value < 3;
+                default:
+                    return false;
+            }
+        }
+
+        private static bool NewLocation(string direction, ref KeyValuePair<int, int> location)
+        {
+            switch (direction)
+            {
+                case "U":
+                    location = new KeyValuePair<int, int>(location.Key - 1, location.Value);
+                    break;
+                case "D":
+                    location = new KeyValuePair<int, int>(location.Key + 1, location.Value);
+                    break;
+                case "L":
+                    location = new KeyValuePair<int, int>(location.Key, location.Value - 1);
+                    break;
+                case "R":
+                    location = new KeyValuePair<int, int>(location.Key, location.Value + 1);
+                    break;
+            }
+            if (location.Key == 3 && location.Value == 3)
+                return true;
+            return false;
+        }
+
+        public static bool[] CheckDoors(string input)
+        {
+            bool[] returnValue = new bool[4];
+            Regex regex = new Regex(@"[bcdef]");
+            using (MD5 md5hash = MD5.Create())
+            {
+                byte[] data = md5hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < 4; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                    returnValue[i] = regex.IsMatch(sBuilder[i].ToString());
+                }
+                return returnValue;
+            }
+        }
+    }
+}
