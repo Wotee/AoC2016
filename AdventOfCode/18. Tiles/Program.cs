a8b5699ffec388affcefe3983885c0b7d2e8db36
Valtteri Tarvus <valtteri.tarvus@liaison.com>
2017-01-14 12:38:11 +0200

    Problem 18

---
+++ AdventOfCode/18. Tiles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace _18.Tiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            char[] input = args.Length != 0 ? args[0].ToCharArray() : ".^^^.^.^^^^^..^^^..^..^..^^..^.^.^.^^.^^....^.^...^.^^.^^.^^..^^..^.^..^^^.^^...^...^^....^^.^^^^^^^".ToCharArray(); // Puzzle input via paramter or hardcoded my default value
+            int safeTiles = 0;
+            for (int i = 0; i < 400000; i++)
+            {
+                if (i == 40)
+                    Console.WriteLine(safeTiles);
+                char[] nextLine = new char[input.Length];
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[j] == '.')
+                        safeTiles++;
+                    bool leftTrap = j == 0 ? false : input[j - 1] == '^';
+                    bool rightTrap = j == input.Length - 1 ? false : input[j + 1] == '^';
+                    nextLine[j] = leftTrap ^ rightTrap ? '^' : '.';
+                }
+                input = nextLine;
+            }
+            Console.WriteLine(safeTiles);
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed.TotalSeconds);
+        }
+    }
+}
